ed195f072dbdcfd8b4eee1f829a30214d64eeb87
anish0m <mohsina.tabassum.rifa@gmail.com>
2024-12-13 16:14:29 +0600

    Python Evaluation

---
+++ Python Evaluation.py
@@ -0,0 +1,16 @@
+def testcase():
+    # solution by anish0m
+    eval(input())
+    return
+
+
+
+def main():
+    t = 1
+    for _ in range(t):
+        testcase()
+
+
+
+if __name__ == "__main__":
+    main()
