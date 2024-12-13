dd72bd919565d3dcd07aef646f984ec84a25514c
anish0m <mohsina.tabassum.rifa@gmail.com>
2024-12-13 16:10:05 +0600

    Input()

---
+++ Input().py
@@ -0,0 +1,17 @@
+def testcase():
+    # solution by anish0m
+    x, k = map(int, input().split())
+    print(eval(input()) == k) 
+    return
+
+
+
+def main():
+    t = 1
+    for _ in range(t):
+        testcase()
+
+
+
+if __name__ == "__main__":
+    main()
