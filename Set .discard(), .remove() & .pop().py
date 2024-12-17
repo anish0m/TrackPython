2836f40a50d2887baeb76d01d6412fb7f4714c2f
anish0m <mohsina.tabassum.rifa@gmail.com>
2024-12-17 11:33:57 +0600

    Set .discard(), .remove() & .pop()

---
+++ Set .discard(), .remove() & .pop().py
@@ -0,0 +1,30 @@
+def testcase():
+    #solution by anish0m
+    n = int(input())
+    st = list(map(int, input().split()))
+
+    for _ in range(int(input())):
+        s = input().split()
+        
+        if s[0] == 'pop':
+            if st:
+                st.pop(0)
+        elif s[0] == 'remove':
+            val = int(s[1])
+            if val in st:
+                st.remove(val)
+        elif s[0] == 'discard':
+            val = int(s[1])
+            if val in st:
+                st.remove(val)
+
+    print(sum(st))
+    return
+
+def main():
+    t = 1  # Single test case
+    for _ in range(t):
+        testcase()
+
+if __name__ == "__main__":
+    main()
